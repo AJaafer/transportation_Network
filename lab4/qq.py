81dc1539fa9f3b1fab012883ee76c6127d170a97
xingengwang <xingengwang@gmail.com>
2017-02-27 14:11:35 -0600

    update lab4

--- lab4/qq.py
+++
@@ -1,8 +0,0 @@
-﻿from Crypto.PublicKey import RSA
-from Crypto import Random
-random_generator = Random.new().read
-key = RSA.generate(1024, random_generator)
-key
-public_key = key.publickey()
-enc_data = public_key.encrypt('abcdefgh', 32)
-enc_data
